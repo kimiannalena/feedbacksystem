06cc74627ea1ca43219b700d88db34dd681cc020
Lukas Schmitt <Lukas.Schmitt-2@mni.thm.de>
2020-06-02 08:45:58 +0200

    revert working state

--- web-gui/src/app/app.module.ts
+++ web-gui/src/app/app.module.ts
@@ -100,8 +100,8 @@ import {CloseTicketDialogComponent} from './components/courses/detail-ticket/clo
 export class ApiURIHttpInterceptor implements HttpInterceptor {
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const clonedRequest: HttpRequest<any> = req.clone({
-      //url: (req.url.search('localhost') >= 0) ? req.url : 'https://localhost:8080'  + req.url // 'https://fk-server.mni.thm.de'
-       url: 'https://feedback.mni.thm.de/'  + req.url // 'https://fk-server.mni.thm.de'
+      url: (req.url.search('localhost') >= 0) ? req.url : 'https://localhost:8080'  + req.url // 'https://fk-server.mni.thm.de'
+      // url: 'https://feedback.mni.thm.de/'  + req.url // 'https://fk-server.mni.thm.de'
     });
 
     return next.handle(clonedRequest);
@@ -212,10 +212,8 @@ export const httpInterceptorProviders = [
     DeleteTaskModalComponent, AnswerFromTestsystemDialogComponent, DeleteSettingDialogComponent,
     CourseParameterModalComponent, CourseParameterUserModalComponent, UploadPlagiatScriptComponent,
     EditTestsystemsModalComponent, DeleteTestsystemAskModalComponent],
-  providers: [CookieService,httpInterceptorProviders],
+  providers: [CookieService],
   bootstrap: [AppComponent]
-
-
 })
 export class AppModule {
 }
